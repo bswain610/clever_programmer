d0e8990dc5417368c1b5a64d795db8f808e119c1
binod kumar swain <bswain610@gmail.com>
2019-11-15 11:21:28 +0530

    new line added

---
+++ home.js
@@ -0,0 +1,23 @@
+//console.log('hello buddy!');
+//var b = 'binod';
+//console.log(b);
+//manipulate DOM with javascript 
+//var age = prompt('what is your age');
+//document.getElementById('sometext').innerHTML= age;
+//NUMBERs with javascript
+//var num = 3.5;
+//num1 = num+1;
+//console.log(num1);
+//var x = parseInt("8 *4*7");
+//alert(x);
+
+function greeting() {
+        var name = prompt('what is your name');
+        var result = 'hello'+' '+ name;
+console.log(result);
+}
+greeting();
+function fun(){
+    alert('this is your function');
+}
+fun();
